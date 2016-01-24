576b744b31162dc1a9f9a5acf211f44851bcc493
Erik Hugne <erik.hugne@gmail.com>
2016-01-24 21:26:10 +0100

    kdev: fix up 9pm network startup script
    Signed-off-by: Erik Hugne <erik.hugne@gmail.com>

--- test/net.tcl
+++ test/net.tcl
@@ -1,19 +1,43 @@
 #!/usr/bin/tclsh
 
-# Source the 9pm library
 package require 9pm
 
-# Open a shell with a name of our choice, lets call it "foobar"
 9pm::shell::open "virsh"
 
 
-set test [9pm::cmd::execute "virsh net-destroy default"]
-puts "$test"
-set test [9pm::cmd::execute "virsh net-undefine default"]
-puts "$test"
-set test [9pm::cmd::execute "virsh net-define default.xml"]
-puts "$test"
-set test [9pm::cmd::execute "virsh net-start default"]
-puts "$test"
-set test [9pm::cmd::execute "virsh net-autostart default"]
-puts "$test"
+proc kill_net {} {
+    9pm::cmd::start "virsh net-info default"
+    expect {
+        -re {Active:\s*yes} {
+        9pm::cmd::finish
+        9pm::cmd::execute "virsh net-destroy default"
+        9pm::output::ok "Network default have been destroyed"
+        kill_net
+        }
+        -re {Active:\s*no} {
+        9pm::cmd::finish
+        9pm::cmd::execute "virsh net-undefine default"
+	9pm::output::ok "Network default have been undefined"
+        return
+        }
+    }
+    set code [9pm::cmd::finish]
+    9pm::output::warning "Network default could not be killed, virsh returned $code"
+}
+
+proc start_net {} {
+    9pm::cmd::execute "virsh net-define default.xml"
+    if {${?} != 0} {
+        9pm::fatal 9pm::output::error "Network could not be defined" 
+    }
+    9pm::output::ok "Network defined from default.xml"
+    9pm::cmd::execute "virsh net-start default"
+    if {${?} != 0} {
+       9pm::fatal 9pm::output::error "Network could not be started" 
+    }
+    9pm::output::ok "Network default started"
+#   9pm::cmd::execute "virsh net-autostart default"
+}
+
+kill_net
+start_net
