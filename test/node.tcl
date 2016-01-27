17272337aff045fe18bb8bf38a2855e861c20270
Erik Hugne <erik.hugne@gmail.com>
2016-01-27 20:59:20 +0100

    kdev: fix node kill function
    with proper recursive call
    
    Signed-off-by: Erik Hugne <erik.hugne@gmail.com>

--- test/node.tcl
+++ test/node.tcl
@@ -5,14 +5,14 @@ package require 9pm
 9pm::shell::open "virsh"
 
 proc kill_domain {domain} {
+    9pm::output::ok "Kill it"
     9pm::cmd::start "virsh dominfo node1"
-    #TODO: there's a race here, if the script is run multiple times in rapid succession
     expect {
         -re {State:\s*running} {
             9pm::cmd::finish
             9pm::cmd::execute "virsh destroy $domain"
             9pm::output::ok "Domain $domain have been destroyed"
-            kill_domain $domain
+            return [kill_domain $domain]
         }
         -re {State:\s*shut off} {
             9pm::cmd::finish
@@ -26,6 +26,7 @@ proc kill_domain {domain} {
 }
 
 proc start_domain {domain xml} {
+    9pm::output::ok "define node"
     9pm::cmd::execute "virsh define $xml"
     if {${?} != 0} {
         9pm::fatal 9pm::output::error "Failed to define domain $domain from $xml"
