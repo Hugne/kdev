520b88d92d99ec7c9d615a679c37f14b460ec2cc
Erik Hugne <erik.hugne@gmail.com>
2016-02-04 17:41:37 +0100

    kdev: more dynamic behavior for net-stop
    if the net is not active, just define it (plan 1)
    if it's active, destroy it first (plan 2)
    
    Signed-off-by: Erik Hugne <erik.hugne@gmail.com>

--- test/cases/net_stop.tcl
+++ test/cases/net_stop.tcl
@@ -9,20 +9,42 @@ proc kill_net {} {
     expect {
         -re {Active:\s*yes} {
         9pm::cmd::finish
-        9pm::cmd::execute "virsh net-destroy default"
-        9pm::output::ok "Network default have been destroyed"
-        return [kill_net]
+        9pm::output::plan 2
+        destroy_net
+        undefine_net
+        return
         }
         -re {Active:\s*no} {
         9pm::cmd::finish
-        9pm::cmd::execute "virsh net-undefine default"
-	9pm::output::ok "Network default have been undefined"
+        9pm::output::plan 1
+        9pm::output::info "Network default is not active"
+        undefine_net
         return
+        }
+        -re {no network with matching name} {
+        9pm::cmd::finish
+        9pm::output::plan 0
+        9pm::output::info "Network default is not defined"
         }
     }
     set code [9pm::cmd::finish]
     9pm::output::warning "Network default could not be killed, virsh returned $code"
 }
 
-9pm::output::plan 2
+proc destroy_net {} {
+    9pm::cmd::execute "virsh net-destroy default"
+    if {${?} != 0} {
+        9pm::fatal 9pm::output::error "Network could not be destroyed"
+    }
+    9pm::output::ok "Network default have been destroyed"
+}
+
+proc undefine_net {} {
+    9pm::cmd::execute "virsh net-undefine default"
+    if {${?} != 0} {
+        9pm::fatal 9pm::output::error "Network could not be undefined"
+    }
+    9pm::output::ok "Network default have been undefined"
+
+}
 kill_net
