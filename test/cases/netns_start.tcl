df2e51b89b0c63e8e953f6be83b1f850615dc4d8
Erik Hugne <erik.hugne@gmail.com>
2019-03-05 21:38:41 +0100

    kdev: netns - add veth devices to bridge

--- test/cases/netns_start.tcl
+++ test/cases/netns_start.tcl
@@ -5,6 +5,15 @@ package require 9pm
 9pm::shell::open "kdev"
 9pm::ssh::connect machine 
 
+proc create_bridge { name } {
+    9pm::cmd::execute "brctl addbr $name"
+    if {${?} != 0} {
+        9pm::fatal 9pm::output::error "Failed to create container bridge $name"
+    }
+    9pm::output::ok "Created container bridge $name"
+
+}
+
 proc create_ns { name } {
     9pm::cmd::execute "ip netns add $name"
     if {${?} != 0} {
@@ -13,7 +22,7 @@ proc create_ns { name } {
     9pm::output::ok "Created network namespace $name"
 }
 
-proc create_veth { name }  {
+proc create_veth { name bridge }  {
     9pm::cmd::execute "ip link add $name type veth peer name veth1"
     if {${?} != 0} {
         9pm::fatal 9pm::output::error "Failed to create veth device pair"
@@ -25,16 +34,22 @@ proc create_veth { name }  {
     }
     9pm::output::ok "Veth device moved to namespace $name"
 
+    9pm::cmd::execute "brctl addif $bridge $name"
+    if {${?} != 0} {
+	 9pm::fatal 9pm::output::error "Failed to add device $name to bridge $bridge"
+    }
+    9pm::output::ok "Veth device $name added to bridge $bridge"
 }
 
 
 set timeout 120
 
 set namespaces {red green blue yellow}
-9pm::output::plan [expr [llength $namespaces]*3]
+9pm::output::plan [expr [llength $namespaces]*4+1]
 set timeout 120
 
+create_bridge "rainbow"
 foreach ns $namespaces {
     create_ns $ns
-    create_veth $ns
+    create_veth $ns "rainbow"
 }
--- test/cases/netns_stop.tcl
+++ test/cases/netns_stop.tcl
@@ -13,12 +13,22 @@ proc destroy_ns { name } {
     9pm::output::ok "Created network namespace $name"
 }
 
+proc destroy_bridge { name } {
+    9pm::cmd::execute "brctl delbr $name"
+    if {${?} != 0} {
+        9pm::fatal 9pm::output::error "Failed to delete container bridge $name"
+    }
+    9pm::output::ok "Container bridge $name deleted"
+
+}
+
 set timeout 120
 
 set namespaces {"red" "green" "blue" "yellow"}
-9pm::output::plan [llength $namespaces]
+9pm::output::plan [expr [llength $namespaces]+1]
 set timeout 120
 
 foreach ns $namespaces {
     destroy_ns $ns
 }
+destroy_bridge "rainbow"
